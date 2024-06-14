0aa93d15e9e6244448d76722760300682931214a
Luana leal <luana46922396@edu.pa.senac.br>
2024-06-13 21:22:19 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Net.Security;
+
+namespace console1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Usuario usuario = new Usuario();
+            usuario.nome = "Eduardo";
+            Console.WriteLine("Olá, " + usuario.nome);
+
+            //
+            string senha;
+            string login;
+            do{
+
+                Console.WriteLine("Digite seu login: ");
+                login = Console.ReadLine();
+
+                Console.WriteLine("Agora digite sua senha: ");
+                senha = Console.ReadLine();
+                
+            }while (usuario.logar(login, senha));
+
+            /*
+            //Método para escrever na tela           
+            Console.WriteLine("Hello, " + usuario.nome);
+
+            Console.WriteLine(usuario.logar("root", "qwe123"));
+
+            string result = Console.ReadLine();
+
+            //Método para ler do teclado
+            Console.WriteLine("O usuario digitou: " +  result);
+            */
+        }
+    }
+}
+  
---
+++ Usuario.cs
@@ -0,0 +1,27 @@
+﻿namespace console1
+{
+    internal class Usuario
+    {
+        //os atributos: nome, login e senha
+        //cadeia = string
+        public string nome;
+        public string login;
+        public string senha;
+        public int qtdlogin;
+        public float peso;
+        public bool esSolteiro;
+        public char abvNomeMeio;
+
+        public bool logar(string _login, string _senha){ 
+        {
+            if (_login == login)
+            {
+                if (_senha == senha)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
